769791b0dcda0c85d5710e76101117dab637b1fb
PeerM1204 <159874446+PeerM1204@users.noreply.github.com>
2024-03-18 11:04:51 +0500

    calculator

---
+++ index.ts
@@ -0,0 +1,99 @@
+import inquirer from "inquirer";
+async function calculator() {
+const answer = await inquirer.prompt([
+  {
+    name: "num1",
+    type: "number",
+    message: "Enter Your First Number:",
+  },
+  {
+    name: "num2",
+    type: "number",
+    message: "Enter Your Second Number",
+  },
+  {
+    name: "operator",
+    type: "list",
+    message: "select the operator you want to perform",
+    choices: ["+", "-", "*", "/", "**", "%"],
+  },
+]);
+const { num1, num2, operator } = answer;
+let result: number;
+
+//for addition:
+if (answer.operator === "+") {
+  result = num1 + num2;
+  console.log(`Your Answer is: ${result}`);
+
+  // for subtraction:
+} else if (answer.operator === "-") {
+  result = num1 - num2;
+  console.log(`Your Answer is:${result}`);
+
+  //for multiplication:
+} else if (answer.operator === "*") {
+  result = num1 * num2;
+  console.log(`Your Answer is:${result}`);
+
+// for Division:
+} else if (answer.operator === "/") {
+  if (num2 !== 0) {
+    result = num1 / num2;
+  console.log(`Your Answer is: ${result}`);
+ } else {
+  console.log("Error: Division by zero!");
+  return;
+}
+
+  // for Exponetiation:
+} else if (answer.operator === "**") {
+  result = num1 ** num2;
+  console.log(`Your Answer is: ${result}`);
+  
+  // for Modulus:
+} else if (answer.operator === "%") {
+  result = num1 % num2;
+  console.log(`Your Answer is: ${result}`);
+} else {
+  console.log("Error: Please select a correct operator.");
+  return;
+}
+};
+
+
+calculator();
+
+// Profile Data Collection:
+
+// import inquirer from "inquirer";
+// async function Profile() {
+//   const data = await inquirer.prompt([
+//     {
+//       name:'profile',
+//       type:'input',
+//       message:'FirstName'
+//     },
+//     {
+//       name:'profile1',
+//       type:'input',
+//       message:'LastName'
+//     },
+//     {
+//       name:'email',
+//       type:'input',
+//       message:'Enter Your Email Address:'
+//     },
+//     {
+//       name:'password',
+//       type:'password',
+//       message:'Enter your password:'
+//     },
+//   ])
+//   let name = (`@${data.profile + data.profile1}`);
+//   console.log(name + name.length);
+//   let mail = (`${data.email + data.password}`);
+  
+// }
+
+// Profile();
